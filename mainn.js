343646c17b540f97e4e522f866c9261c6b91d25b
BorisBoko <borispetrosyans8@gmail.com>
2020-12-25 16:01:26 +0400

    finish

--- mainn.js
+++ mainn.js
@@ -12,46 +12,78 @@ let picsUrl = [
     "9.jpg",
 ]
 let shuffledd = []
+let clickPics = []
+let checkSrc = []
+
+
 function randomMy() {
-     shuffledd = picsUrl
-    .map((a) => ({ sort: Math.random(), value: a }))
-    .sort((a, b) => a.sort - b.sort)
-    .map((a) => a.value)
-   
+    shuffledd = picsUrl
+        .map((a) => ({ sort: Math.random(), value: a }))
+        .sort((a, b) => a.sort - b.sort)
+        .map((a) => a.value)
+
 }
 randomMy()
 
 let playfield = [
-    [0, 0, 0],
-    [0, 0, 0],
-    [0, 0, 0]
+    shuffledd[0], shuffledd[1], shuffledd[2],
+    shuffledd[3], shuffledd[4], shuffledd[5],
+    shuffledd[6], shuffledd[7], shuffledd[8]
 ];
-
-
-function draw() {
+// drow UI
+function draw(playfield) {
     let sum = -1
     let mainInnerHTML = '';
     for (let y = 0; y < playfield.length; y++) {
-        for (let x = 0; x < playfield[y].length; x++) {
-                 sum++
-                mainInnerHTML += `<img src="${shuffledd[sum]}" alt="">`;
-        }
+        sum++
+        mainInnerHTML += `<img src="${playfield[sum]}" alt="">`;
     }
-
     main.innerHTML = mainInnerHTML;
 };
+draw(playfield);
 
- draw();
- let a = []
-const onclick =  function (event){
-   
+const onclick = function(event) {
     const click = event.target.attributes.src.nodeValue
-    a.push(click)
-    console.log(shuffledd)
 
-    console.log(a)
-    draw();
 
+    if (check(click)) {
+        draw(playfield)
+    }
 }
 
-main.addEventListener('click',onclick)
+//Event in click
+main.addEventListener('click', onclick)
+
+//check secound click is Empty place
+function check(click) {
+    clickPics.push(click)
+    checkSrc = Array.from(new Set(clickPics))
+
+    if (checkSrc.length === 2 || checkSrc[0] === "9.jpg") {
+        clickPics = []
+    }
+
+    let first = playfield.indexOf(checkSrc[0])
+    let secound = playfield.indexOf(checkSrc[1])
+
+    if ((checkSrc[1] === "9.jpg")) {
+        if (checkNear(first, secound)) {
+            playfield[secound] = playfield[first]
+            playfield[first] = "9.jpg"
+            return true
+        }
+
+    }
+}
+//check first click is near secoud click
+function checkNear(first, secound) {
+
+    if (first - secound === 1 || first - secound === -1 || first - secound === -3 || first - secound === 3) {
+        if (first === 5 && secound === 6) { return false }
+        if (first === 6 && secound === 5) { return false }
+        if (first === 2 && secound === 3) { return false }
+        if (first === 3 && secound === 2) { return false }
+
+        return true
+    }
+}
